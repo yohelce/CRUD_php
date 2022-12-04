ce80f31496ae196af975bf8ec4637d6aae740f1d
yohelce <yohel.cruz.espinoza@gmail.com>
2022-12-04 01:24:15 -0500

    Added save_local.php for POST new local

---
+++ save_local.php
@@ -0,0 +1,20 @@
+<?php
+include("db.php");
+
+if (isset($_POST['save_local'])){
+    $local = $_POST['local'];
+    $address =  $_POST['address'];
+
+    $query = "INSERT INTO locales(local_name, local_address) VALUES('$local', '$address')";
+    $result = mysqli_query($conn, $query);
+    if (!$result) {
+        die("Query failed");
+    }
+
+    $_SESSION['message'] =  'Local Saved successfully';
+    $_SESSION['message_type'] = 'success';
+
+    header("Location: index.php");
+}
+
+?>
